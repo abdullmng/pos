a4f16839378dfe44e83a2d30396311517e4cc6b5
Muhammad Abdullahi <abdullmng@gmail.com>
2024-01-28 08:56:56 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,23 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/payment-flow/chart-data', 'HomeController@paymentChart')
         ->name('payment-flow.chart');
+
+
+        Route::get("/storage/{model_id}/{filename}", function ($model_id, $filename)
+        {
+            // Add folder path here instead of storing in the database.
+            $path = storage_path("app/public/{$model_id}/" . $filename);
+
+            if (!File::exists($path)) {
+                abort(404);
+            }
+
+            $file = File::get($path);
+            $type = File::mimeType($path);
+
+            $response = Response::make($file, 200);
+            $response->header("Content-Type", $type);
+            return $response;
+        });
 });
 
